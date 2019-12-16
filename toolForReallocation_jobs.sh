a0514ebf80c42a8dc1370ff485cbbad4a2538ae9
yuanxiaolu <leavenyuan@yahoo.com>
2019-12-16 14:32:10 +0800

    update for concurrent

---
+++ toolForReallocation_jobs.sh
@@ -0,0 +1,30 @@
+BASE_PATH=/data/测试人像库数据/formatted
+cd $BASE_PATH
+TOTAL_NUM=`ls -l | grep "^-" | wc -l`
+EACH_NUM=10000
+FOLDER_NUM=$[$[$TOTAL_NUM/$EACH_NUM]+1]
+echo $FOLDER_NUM
+FILE_BASE_NAME="IT001-001-00000000.jpg"
+FOLDER_BASE_NAME="IMAGE_10k_FOLDER_"
+
+#create folders
+#based on how many files you want to store in each folder
+cd ..
+for i in $(seq 1 $FOLDER_NUM);
+do
+    #mkdir ${BASE_PATH}/${FOLDER_BASE_NAME}${i} &
+    mkdir ${FOLDER_BASE_NAME}${i} 
+done
+
+#copy files to each folder
+for k in $(seq 0 $[$FOLDER_NUM-1]);
+do
+echo $k
+    for i in $(seq $[$[$k*$EACH_NUM]+1] $[$[$k+1]*$EACH_NUM]); 
+        do 
+        INDEX=`printf "%08d\n" ${i}`
+        echo IT001-001-${INDEX}.jpg
+        #cp ${BASE_PATH}/IT001-001-${INDEX}.jpg ${BASE_PATH}/IMAGE_FOLDER_$[$k+1]/ & 
+        cp ${BASE_PATH}/IT001-001-${INDEX}.jpg ./IMAGE_FOLDER_$[$k+1]/ & 
+        done
+done
